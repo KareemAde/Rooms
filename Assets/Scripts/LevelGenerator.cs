ff4a145e4784f117b121d0b23ad7da0cf875b143
KareemAde <37348535+KareemAde@users.noreply.github.com>
2019-01-27 12:26:33 -0500

    Initial Commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    public GameObject levelblock;
+    public int size;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Instantiate(levelblock, new Vector3(-4 * j, -4 * i, 0), Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody player;
+    public bool grounded = true;
+    public bool wall = false;
+    KeyCode jump = KeyCode.UpArrow;
+
+    [Range(1, 50)]
+    public float speed;
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    // Use this for initialization
+    void Start () {
+        player = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        Debug.Log(moveHorizontal);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f) * Time.deltaTime * speed;
+        player.AddForce(movement * speed);
+
+        //player.transform.Translate(moveHorizontal * Time.deltaTime * speed, 0.0f, 0.0f);
+        //player.MovePosition(transform.position + movement);
+
+        if (Input.GetKeyDown(jump) && grounded) 
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpVelocity;
+            grounded = !grounded;
+        }
+
+        if (Input.GetKeyDown(jump) && wall)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpVelocity;
+        }
+
+        if (wall)
+            moveHorizontal = moveHorizontal * -1;
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Floor"))
+        {
+            grounded = true;
+        }
+
+        if (other.collider.CompareTag("Wall") && !grounded) 
+        {
+            wall = true;
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        /*if (other.collider.CompareTag("Floor"))
+        {
+            grounded = false;
+        }*/
+
+        if (other.collider.CompareTag("Wall"))
+        {
+            wall = false;
+        }
+    }
+}
